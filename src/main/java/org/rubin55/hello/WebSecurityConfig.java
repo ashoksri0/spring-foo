e3c2ae5391323f092c5dc2f5030ed4ecbb72a17b
Rubin Simons <rubin@raaftech.com>
2017-05-06 20:14:23 +0200

    Added two example ldifs, rubin:SHA, freek:SSHA.

---
+++ src/main/java/org/rubin55/hello/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package org.rubin55.hello;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .anyRequest()
+        .fullyAuthenticated()
+        .and()
+        .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+        .userDnPatterns("uid={0},ou=people")
+        .contextSource(contextSource())
+        .passwordCompare()
+        .passwordEncoder(new LdapShaPasswordEncoder())
+        .passwordAttribute("userPassword");
+    }
+
+    @Bean
+    public DefaultSpringSecurityContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:10389/"), "dc=example,dc=com");
+    }
+}
